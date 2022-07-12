842f94c2b0f8d73428919a3c9f397720554c30a7
jkushner <jakub.kuszneruk@neptune.ai>
2022-07-12 23:53:32 +0200

    Revert "DDT and VS config"
    This reverts commit 970808460821d2d144fa6c52ef5cc5988cac22ef.

--- 550 - Testing/tests/step_defs/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from decoder_exercise_solution import Decoder
-
-
-@pytest.fixture
-def decoder():
-    yield Decoder()
--- tmp.py
+++
@@ -1,6 +0,0 @@
-a = 10
-b = 20
-
-if condition := a + b == 30:
-    print(condition)
-    # do something
